cd45787a8497f8d4d4161909545478a7f298cb00
jhonatapeixoto <44884231+jhonatapeixoto@users.noreply.github.com>
2018-11-08 22:36:12 -0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,14 @@
+
+
+// Camarada clicou no botão, guardar a 
+// string que existia dentro do input
+
+document.getElementById('Add').addEventListener('click', function() {
+    var texto=document.getElementById('Item').value;
+    console.log(texto);
+
+    if(texto){
+        
+    }
+
+})
